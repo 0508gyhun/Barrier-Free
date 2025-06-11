721ea1cf4460a256433da31d6dc92998b1df46c5
0508gyhun <0508gyhun@naver.com>
2025-06-11 17:19:41 +0900

    뷰 바인딩 적용

---
+++ app/src/main/java/com/example/barrier_free_restaurant/SignInFragment.kt
@@ -0,0 +1,28 @@
+package com.example.barrier_free_restaurant
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.barrier_free_restaurant.databinding.FragmentSignInBinding
+
+class SignInFragment: Fragment() {
+
+    private var _binding : FragmentSignInBinding? = null
+    private val binding get() = _binding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSignInBinding.inflate(inflater,container,false)
+        return binding.root!!
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
--- app/src/main/java/com/example/barrier_free_restaurant/SignUpFragment.kt
+++ app/src/main/java/com/example/barrier_free_restaurant/SignUpFragment.kt
@@ -2,5 +2,5 @@ package com.example.barrier_free_restaurant
 
 import androidx.fragment.app.Fragment
 
-class CreateAccountFragment: Fragment(R.layout.fragment_create_account) {
+class SignUpFragment: Fragment() {
 }
