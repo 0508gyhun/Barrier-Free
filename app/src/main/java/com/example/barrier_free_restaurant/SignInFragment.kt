34149a9f7cc22b10036731a22ad7f5ecfd829a56
0508gyhun <0508gyhun@naver.com>
2025-06-11 19:51:33 +0900

    firebase auth 연동, jetpack nav 구현, safe arg 구현

--- app/src/main/java/com/example/barrier_free_restaurant/SignInFragment.kt
+++ app/src/main/java/com/example/barrier_free_restaurant/SignInFragment.kt
@@ -4,25 +4,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.barrier_free_restaurant.databinding.FragmentSignInBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
-class SignInFragment: Fragment() {
+class SignInFragment : Fragment() {
 
-    private var _binding : FragmentSignInBinding? = null
+    private var _binding: FragmentSignInBinding? = null
     private val binding get() = _binding!!
+
+    private var auth: FirebaseAuth = FirebaseAuth.getInstance()
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentSignInBinding.inflate(inflater,container,false)
+        _binding = FragmentSignInBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        setOnClickListener()
+    }
 
+    private fun setOnClickListener() {
+        binding.btnCreateAccount.setOnClickListener {
+            val action = SignInFragmentDirections.actionSignInFragmentToSignUpFragment()
+            findNavController().navigate(action)
+        }
     }
 }
