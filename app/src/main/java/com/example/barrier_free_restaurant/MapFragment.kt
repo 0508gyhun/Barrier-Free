dd365f0a3bfdd94eec5abfd007c1c5b20b7f215b
0508gyhun <0508gyhun@naver.com>
2025-06-16 12:23:20 +0900

    map화면 bottom sheet에 들어갈 card 구현

--- app/src/main/java/com/example/barrier_free_restaurant/MapFragment.kt
+++ app/src/main/java/com/example/barrier_free_restaurant/MapFragment.kt
@@ -1,6 +1,25 @@
 package com.example.barrier_free_restaurant
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.naver.maps.map.LocationTrackingMode
+import com.naver.maps.map.NaverMap
+import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.UiSettings
+import com.naver.maps.map.util.FusedLocationSource
 
-class MapFragment : Fragment(R.layout.fragment_map){
+class MapFragment : Fragment(R.layout.fragment_map), OnMapReadyCallback{
+    private lateinit var locationSource: FusedLocationSource
+    private lateinit var naverMap: NaverMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onMapReady(p0: NaverMap) {
+        this.naverMap = p0
+        naverMap.locationSource = locationSource
+
+    }
+
 }
