016afc3cd133a596005755a83841a7b26daf6535
0508gyhun <0508gyhun@naver.com>
2025-06-16 13:11:50 +0900

    map화면 bottom sheet 나오도록 구현

--- app/src/main/java/com/example/barrier_free_restaurant/MapFragment.kt
+++ app/src/main/java/com/example/barrier_free_restaurant/MapFragment.kt
@@ -1,14 +1,24 @@
 package com.example.barrier_free_restaurant
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.example.barrier_free_restaurant.databinding.FragmentMapBinding
+import com.example.barrier_free_restaurant.databinding.FragmentSearchBinding
 import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.NaverMap
 import com.naver.maps.map.OnMapReadyCallback
 import com.naver.maps.map.UiSettings
 import com.naver.maps.map.util.FusedLocationSource
 
-class MapFragment : Fragment(R.layout.fragment_map), OnMapReadyCallback{
+class MapFragment : Fragment(), OnMapReadyCallback{
+
+    private var _binding : FragmentMapBinding? = null
+    private val binding get() = _binding!!
+
     private lateinit var locationSource: FusedLocationSource
     private lateinit var naverMap: NaverMap
 
@@ -16,10 +26,26 @@ class MapFragment : Fragment(R.layout.fragment_map), OnMapReadyCallback{
         super.onCreate(savedInstanceState)
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMapBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.testBtn.setOnClickListener {
+
+            val modalBottomSheet = ModalBottomSheet()
+            modalBottomSheet.show(parentFragmentManager, ModalBottomSheet.TAG)
+        }
+
+    }
     override fun onMapReady(p0: NaverMap) {
         this.naverMap = p0
-        naverMap.locationSource = locationSource
-
     }
 
 }
