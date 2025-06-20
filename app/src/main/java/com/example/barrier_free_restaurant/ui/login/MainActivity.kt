1102f6335f2a4a8d15deb60dedc36f4363af7df9
0508gyhun <0508gyhun@naver.com>
2025-06-20 16:19:09 +0900

    로그인 후 화면에서 백버튼 2초내에 두번 눌러야 종료되게 구현

--- app/src/main/java/com/example/barrier_free_restaurant/ui/login/MainActivity.kt
+++ app/src/main/java/com/example/barrier_free_restaurant/ui/login/MainActivity.kt
@@ -35,6 +35,7 @@ class MainActivity : AppCompatActivity() {
         if(currentUser != null) {
             val intent = Intent(this,HomeActivity::class.java)
             startActivity(intent)
+            finish()
         }
     }
 }
